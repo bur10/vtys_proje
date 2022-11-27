3a1e1c6b5895f549f01e4418cba4ece25c7ba800
yusuf kutlu <yusufgokalp.kutlu@outlook.com>
2022-11-28 02:24:47 +0300

    probably last version will add flash messages

--- server.py
+++ server.py
@@ -1,19 +1,114 @@
+import random
 from flask import Flask, request, render_template, redirect, url_for, flash
-from takso import add_driver, delete_driver, get_drivers, get_driver_by_name, get_driver_by_id, update_driver
+from takso import Drivers, Customers, Taxis, ActiveVoyages, PastVoyages
+from datetime import datetime
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
+selected_customer = 2
+
+
+# --- ADMIN ROUTES ---
 
 @app.route("/admin")
 def admin_home():
     return render_template("admin/index_admin.html")
+
+# --- TAXI ROUTES ---
+
+
+@app.route('/admin/taksi_ekle', methods=['GET', 'POST'])
+def admin_add_taxi():
+    if request.method == "POST":
+        print(request.form['plate_number'])
+        Taxis.add_taxi(
+            plate_number=request.form['plate_number'],
+            car_model=request.form['car_model'],
+            car_year=request.form['car_year']
+        )
+        return redirect(url_for('admin_add_taxi'))
+    else:
+        return render_template("admin/taxi/taksi_ekle.html")
+
+
+@app.route("/admin/taksi_sil", methods=['GET', 'POST'])
+def admin_delete_taxi():
+    if request.method == "POST":
+        print(request.form['plate_number'])
+        Taxis.delete_taxi(plate_number=request.form['plate_number'])
+        return redirect(url_for('admin_delete_taxi'))
+    else:
+        return render_template("admin/taxi/taksi_sil.html")
+
+
+@app.route("/admin/taksi_sil/<int:id>", methods=['GET'])
+def admin_delete_taxi_by_id(id):
+    Taxis.delete_taxi(id=id)
+    return redirect(url_for('admin_show_taxis'))
+
+
+@app.route("/admin/taksi_goster", methods=['GET'])
+def admin_show_taxis():
+    return render_template("admin/taxi/taksi_goster.html", taxis=Taxis.get_all_taxis())
+
+# --- CUSTOMER ROUTES ---
+
+
+@app.route('/admin/musteri_ekle', methods=['GET', 'POST'])
+def admin_add_customer():
+    if request.method == "POST":
+        print(request.form['name'])
+        Customers.add_customer(
+            name=request.form['name'],
+            last_name=request.form['last_name'],
+            phone_number=request.form['phone_number'],
+            address=request.form['address']
+        )
+        return redirect(url_for('admin_add_customer'))
+    else:
+        return render_template("admin/customer/musteri_ekle.html")
+
+
+@app.route('/admin/musteri_duzenle/<int:id>', methods=['GET', 'POST'])
+def admin_update_customer(id):
+    if request.method == 'POST':
+        print(id)
+        Customers.update_customer(
+            id=id,
+            name=request.form['name'],
+            last_name=request.form['last_name'],
+            phone_number=request.form['phone_number'],
+            address=request.form['address']
+        )
+        return redirect(url_for('admin_update_customer', id=id))
+    else:
+        return render_template("admin/customer/musteri_duzenle.html", customer=Customers.get_customer_by_id(id))
+
+
+@app.route("/admin/musteri_sil/<int:id>", methods=['GET'])
+def admin_delete_customer(id):
+    Customers.delete_customer(id)
+    return redirect(url_for('admin_customer_results'))
+
+
+@app.route("/admin/musteri_ara", methods=['GET', 'POST'])
+def admin_customer_results():
+    if request.method == "POST":
+        return render_template("admin/customer/musteri_ara.html", customers=Customers.get_customer_by_name(request.form['name'], request.form['last_name']))
+    else:
+        return render_template("admin/customer/musteri_ara.html")
+
+# --- END CUSTOMER ROUTES ---
+
+# --- DRIVER ROUTES ---
 
 
 @app.route("/admin/surucu_ekle", methods=['GET', 'POST'])
 def admin_add_driver():
     if request.method == "POST":
         print(request.form['name'])
-        add_driver(
+        Drivers.add_driver(
             name=request.form['name'],
             last_name=request.form['last_name'],
             photo_link=request.form['photo_link'],
@@ -21,41 +116,113 @@ def admin_add_driver():
         )
         return redirect(url_for('admin_add_driver'))
     else:
-        return render_template("admin/surucu_ekle.html")
+        return render_template("admin/driver/surucu_ekle.html")
 
 
 @app.route("/admin/surucu_duzenle/<int:id>", methods=['GET', 'POST'])
 def admin_update_driver(id):
     if request.method == 'POST':
-        print(request.form['id'])
-        update_driver(
-            id=request.form['id'],
+        print(id)
+        Drivers.update_driver(
+            id=id,
             name=request.form['name'],
             last_name=request.form['last_name'],
             photo_link=request.form['photo_link'],
             biography=request.form['biography']
         )
-        return redirect(url_for('admin_update_driver', id=request.form['id']))
+        return redirect(url_for('admin_update_driver', id=id))
     else:
-        return render_template("admin/surucu_duzenle.html", driver=get_driver_by_id(id))
+        return render_template("admin/driver/surucu_duzenle.html", driver=Drivers.get_driver_by_id(id))
+
 
 @app.route("/admin/surucu_sil/<int:id>", methods=['GET'])
 def admin_delete_driver(id):
-    delete_driver(id)
+    Drivers.delete_driver(id)
     return redirect(url_for('admin_driver_results'))
 
 
 @app.route("/admin/surucu_ara", methods=['GET', 'POST'])
 def admin_driver_results():
     if request.method == "POST":
-        return render_template("admin/surucu_ara.html", drivers=get_driver_by_name(request.form['name'], request.form['last_name']))
+        return render_template("admin/driver/surucu_ara.html", drivers=Drivers.get_driver_by_name(request.form['name'], request.form['last_name']))
     else:
-        return render_template("admin/surucu_ara.html")
+        return render_template("admin/driver/surucu_ara.html")
+
+# --- END DRIVER ROUTES ---
+
+# --- END ADMIN ROUTES ---
+
+# --- USER ROUTES ---
 
 
 @app.route("/")
 def home():
-    return render_template("user/index.html", drivers=get_drivers())
+    active_voyage = ActiveVoyages.get_customer_active_voyage(selected_customer)
+    voyage_info = {}
+    if active_voyage != None:
+        voyage_info = {
+            'driver_info': Drivers.get_driver_by_id(active_voyage['driver_id']),
+            'taxi_info': Taxis.get_taxi_by_id(active_voyage['taxi_id']),
+            'where_to': active_voyage['where_to'],
+            'where_from': active_voyage['where_from'],
+        }
+    return render_template("user/index.html", available_drivers=Drivers.get_available_drivers(), voyage_info=voyage_info)
+
+@app.route("/user/profil")
+def user_profile():
+    return render_template("user/profil.html", user=Customers.get_customer_by_id(selected_customer))
+
+
+@app.route("/user/surucu_cagir/<int:driver_id>")
+def call_driver(driver_id):
+    res = ActiveVoyages.add_active_voyage(
+        customer_id=selected_customer,
+        driver_id=driver_id,
+        taxi_id=random.choice([taxi['id'] for taxi in Taxis.get_all_taxis()]),
+        where_to="Teknoloji Fakültesi",
+        where_from="Evden"
+    )
+    if res == -1:
+        flash("Sürücü çağırma başarısız! Zaten aktif bir yolculuğunuz var!", 'error')
+    return redirect(url_for('home'))
+
+@app.route("/user/surusu_bitir", methods=['POST'])
+def end_voyage():
+    voyage = ActiveVoyages.get_customer_active_voyage(selected_customer)
+    time_passed = (datetime.now() - voyage['call_date']).seconds
+    total_try = round((time_passed/60) * 7, 2)
+    if voyage != None:
+        PastVoyages.add_past_voyage(
+            active_voyage_id=voyage['id'],
+            customer_id=voyage['customer_id'],
+            driver_id=voyage['driver_id'],
+            taxi_id=voyage['taxi_id'],
+            where_to=voyage['where_to'],
+            where_from=voyage['where_from'],
+            call_date=voyage['call_date'],
+            end_date=datetime.now(),
+            rating=int(request.form['rating']),
+            total=total_try,
+        )
+    else:
+        print("aktif voyage yok!")
+    return redirect(url_for('home'))
+
+
+@app.route("/user/tum_surusler")
+def all_voyages():
+    voyages = [{"driver_info": Drivers.get_driver_by_id(voyage['driver_id']),
+                "taxi_info": Taxis.get_taxi_by_id(voyage['taxi_id']),
+                "where_to": voyage['where_to'],
+                "where_from": voyage['where_from'],
+                "total": voyage['total'],
+                "rating": voyage['rating'],
+                "total_minutes": round(((voyage["end_date"] - voyage['call_date']).seconds)/60, 2),
+                "call_date": voyage['call_date']} for voyage in PastVoyages.get_all_voyages(selected_customer)]
+    return render_template("user/gecmis_surusler.html", voyages=reversed(voyages))
+
+
+# --- END USER ROUTES ---
 
 
 if __name__ == "__main__":
--- takso.py
+++ takso.py
@@ -1,4 +1,6 @@
 import pyodbc
+import time
+import datetime as dt
 from datetime import datetime
 
 conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
@@ -9,51 +11,169 @@ conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
 cursor = conn.cursor()
 
 
-def add_driver(name, last_name, photo_link, biography):
-    cursor.execute("INSERT INTO Driver(name, last_name, photo_link, biography, registered_date) VALUES (?, ?, ?, ?, ?)",
-                   (name, last_name, photo_link, biography, datetime.now()))
-    conn.commit()
+class Drivers:
+    def add_driver(name, last_name, photo_link, biography):
+        cursor.execute("INSERT INTO Drivers(name, last_name, photo_link, biography, registered_date, total_voyages) VALUES (?, ?, ?, ?, ?, ?)",
+                       (name, last_name, photo_link, biography, datetime.now(), 0))
+        conn.commit()
 
+    def update_driver(id, name, last_name, photo_link, biography):
+        cursor.execute('''UPDATE Drivers
+                        SET name=?, last_name=?, photo_link=?, biography=?
+                        WHERE id=?''',
+                       (name, last_name, photo_link, biography, id))
+        conn.commit()
 
-def update_driver(id, name, last_name, photo_link, biography):
-    cursor.execute('''UPDATE Driver
-                      SET name=?, last_name=?, photo_link=?, biography=?
-                      WHERE id=?''',
-                   (name, last_name, photo_link, biography, id))
-    conn.commit()
+    def delete_driver(id):
+        cursor.execute(f"DELETE FROM Drivers WHERE id = {id}")
+        conn.commit()
 
+    def get_driver_by_name(name, last_name):
+        if last_name != "":
+            cursor.execute(
+                "SELECT * FROM Drivers WHERE name=? and last_name=?", (name, last_name))
+        else:
+            cursor.execute(
+                "SELECT * FROM Drivers WHERE name=?", (name))
 
-def delete_driver(id):
-    cursor.execute(f"DELETE FROM Driver WHERE Driver.id = {id}")
-    conn.commit()
+        columns = [column[0] for column in cursor.description]
+        results = [dict(zip(columns, row)) for row in cursor.fetchall()]
+        return results
 
-
-def get_driver_by_name(name, last_name):
-    if last_name != "":
+    def get_driver_by_id(id):
         cursor.execute(
-            "SELECT * FROM Driver WHERE name=? and last_name=?", (name, last_name))
-    else:
+            "SELECT * FROM Drivers WHERE id=?", (id))
+        columns = [column[0] for column in cursor.description]
+        results = [dict(zip(columns, row)) for row in cursor.fetchall()]
+        return results[0]
+
+    def get_available_drivers():
+        cursor.execute('''SELECT * FROM Drivers
+                        WHERE id NOT IN (SELECT driver_id FROM ActiveVoyages)''')
+        columns = [column[0] for column in cursor.description]
+        results = [dict(zip(columns, row)) for row in cursor.fetchall()]
+        return results
+
+
+class Customers:
+
+    def add_customer(name, last_name, phone_number, address):
+        cursor.execute("INSERT INTO Customers(name, last_name, phone_number, address, registered_date, total_voyages) VALUES (?, ?, ?, ?, ?, ?)",
+                       (name, last_name, phone_number, address, datetime.now(), 0))
+        conn.commit()
+
+    def update_customer(id, name, last_name, phone_number, address):
+        cursor.execute('''UPDATE Customers
+                        SET name=?, last_name=?, phone_number=?, address=?
+                        WHERE id=?''',
+                       (name, last_name, phone_number, address, id))
+        conn.commit()
+
+    def delete_customer(id):
+        cursor.execute(f"DELETE FROM Customers WHERE id = {id}")
+        conn.commit()
+
+    def get_customer_by_name(name, last_name):
+        if last_name != "":
+            cursor.execute(
+                "SELECT * FROM Customers WHERE name=? and last_name=?", (name, last_name))
+        else:
+            cursor.execute(
+                "SELECT * FROM Customers WHERE name=?", (name))
+
+        columns = [column[0] for column in cursor.description]
+        results = [dict(zip(columns, row)) for row in cursor.fetchall()]
+        return results
+
+    def get_customer_by_id(id):
         cursor.execute(
-            "SELECT * FROM Driver WHERE name=?", (name))
+            "SELECT * FROM Customers WHERE id=?", (id))
+        columns = [column[0] for column in cursor.description]
+        results = [dict(zip(columns, row)) for row in cursor.fetchall()]
+        return results[0]
 
-    columns = [column[0] for column in cursor.description]
-    results = [dict(zip(columns, row)) for row in cursor.fetchall()]
-    return results
+
+class Taxis:
+    def add_taxi(plate_number, car_model, car_year):
+        cursor.execute('''INSERT INTO Taxis(plate_number, car_model, car_year, registered_date, total_voyages)
+                        VALUES (?, ?, ?, ?, ?)''',
+                       (plate_number, car_model, car_year, datetime.now(), 0))
+        conn.commit()
+
+    def delete_taxi(id="", plate_number=""):
+        if plate_number != "":
+            cursor.execute('''DELETE FROM Taxis
+                        WHERE plate_number=?''', (plate_number))
+        else:
+            cursor.execute('''DELETE FROM Taxis
+                        WHERE id=?''', (id))
+        conn.commit()
+
+    def get_taxi_by_id(id):
+        cursor.execute("SELECT * FROM Taxis WHERE id=?", (id))
+        columns = [column[0] for column in cursor.description]
+        results = [dict(zip(columns, row)) for row in cursor.fetchall()]
+        return results[0]
+
+    def get_all_taxis():
+        cursor.execute("SELECT * FROM Taxis")
+        columns = [column[0] for column in cursor.description]
+        results = [dict(zip(columns, row)) for row in cursor.fetchall()]
+        return results
 
 
-def get_driver_by_id(id):
-    cursor.execute(
-            "SELECT * FROM Driver WHERE id=?", (id))
-    columns = [column[0] for column in cursor.description]
-    results = [dict(zip(columns, row)) for row in cursor.fetchall()]
-    return results[0]
+class ActiveVoyages:
+    def add_active_voyage(customer_id, driver_id, taxi_id, where_to, where_from):
 
-def get_drivers():
-    cursor.execute("SELECT * FROM Driver")
-    columns = [column[0] for column in cursor.description]
-    results = [dict(zip(columns, row)) for row in cursor.fetchall()]
-    return results
+        cursor.execute(
+            "SELECT customer_id FROM ActiveVoyages WHERE customer_id = ?", (customer_id))
+        if cursor.fetchone() is not None:
+            print("zaten aktif sürüşü var")
+            return -1
+
+        cursor.execute('''INSERT INTO ActiveVoyages
+                        VALUES (?,?,?,?,?,?)''',
+                       (customer_id, driver_id, taxi_id, where_to, where_from, datetime.now()))
+        conn.commit()
+
+    def get_customer_active_voyage(customer_id):
+        cursor.execute(
+            "SELECT * FROM ActiveVoyages WHERE customer_id = ?", (customer_id))
+        columns = [column[0] for column in cursor.description]
+        results = [dict(zip(columns, row)) for row in cursor.fetchall()]
+        print(results)
+        return results[0] if results != [] else None
+
+
+class PastVoyages:
+
+    def add_past_voyage(active_voyage_id, customer_id, driver_id, taxi_id, where_to, where_from, call_date, end_date, rating, total):
+        cursor.execute('''INSERT INTO PastVoyages
+                          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)''', 
+                          (customer_id, driver_id, taxi_id, where_to, where_from, call_date, end_date, rating, total))
+
+        cursor.execute("DELETE FROM ActiveVoyages WHERE id = ?", (active_voyage_id))
+        cursor.execute('''UPDATE Drivers
+                          SET total_voyages = total_voyages + 1, overall_score = (CASE WHEN (overall_score is NULL) THEN ? ELSE (overall_score + ?) / 2 END)
+                          WHERE id = ?''', (rating, rating, driver_id))
+        cursor.execute('''UPDATE Customers
+                          SET total_voyages = total_voyages + 1
+                          WHERE id = ?''', (customer_id))
+        cursor.execute('''UPDATE Taxis
+                          SET total_voyages = total_voyages + 1
+                          WHERE id = ?''', (taxi_id))
+        conn.commit()
+
+    def get_all_voyages(customer_id):
+        cursor.execute("SELECT * FROM PastVoyages WHERE customer_id = ?", (customer_id))
+        columns = [column[0] for column in cursor.description]
+        results = [dict(zip(columns, row)) for row in cursor.fetchall()]
+        return results
 
 
 if __name__ == "__main__":
-    print(get_driver_by_id(11))
+    now = datetime.now()
+    tommorow = datetime.now() + dt.timedelta(minutes=48)
+    print(((tommorow - now).seconds)/60)
+    # print(get_available_drivers())
+    # print([taxi['id'] for taxi in get_all_taxis()])
